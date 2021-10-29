51d0e95af45dcd85388246c78258f1e3111f5914
jose <info@josecampos.io>
2021-10-29 15:34:30 +0200

    images displayed, style done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  currentPage = 0;
+  
   images = [
     {
       title: 'Hauptbahnhof',
